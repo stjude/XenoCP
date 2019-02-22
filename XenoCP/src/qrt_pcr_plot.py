0437afadb2d13a51089f168c30a1a2ad28045cba
Liang Ding <liang.ding@stjude.org>
2019-02-22 17:24:47 -0600

    more plots

--- XenoCP/src/qrt_pcr_plot.py
+++ XenoCP/src/qrt_pcr_plot.py
@@ -8,12 +8,11 @@ import pandas as pd
 sns.set(style="ticks", palette="pastel")
 
 # Load the dataset
-qrt_pcr = pd.read_csv('./XenoCP/data/qRT-PCR.csv')
+qrt_pcr = pd.read_csv('../data/qRT-PCR.csv')
 tips = sns.load_dataset("tips")
 
-ax = sns.boxplot(x="Endpoint tumors analyzed (mice)", y="Expression (logCq)", data=qrt_pcr)
+ax = sns.boxplot(x="Endpoint tumors analyzed (mice)", y="Expression (logCq)", data=qrt_pcr, width=0.5)
 ax = sns.swarmplot(x="Endpoint tumors analyzed (mice)", y="Expression (logCq)", data=qrt_pcr, color=".25")
 plt.title('qRT-PCR')
 plt.show()
-plt.savefig('./XenoCP/data/qRT-PCR.png')
-
+plt.savefig('../data/qRT-PCR.png')
