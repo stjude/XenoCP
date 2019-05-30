8fe7aee09cf1e0d2b802cdbae4670db749299423
Andrew Thrasher <adthrasher@gmail.com>
2019-05-30 11:49:38 -0400

    Defaults for CLASSPATH should not completely override any existing CLASSPATH values.

--- bin/java.sh
+++ bin/java.sh
@@ -3,15 +3,15 @@
 for arg in "$@"; do
     case $arg in
     org.stjude.compbio.sam.*)
-        CLASSPATH=/opt/tools-sam/lib/*
+        CLASSPATH=/opt/tools-sam/lib/*:$CLASSPATH
         break
         ;;
     org.stjude.compbio.xenocp.*)
-        CLASSPATH=/opt/xenocp/lib/*
+        CLASSPATH=/opt/xenocp/lib/*:$CLASSPATH
         break
         ;;
     picard.cmdline.*)
-        CLASSPATH=/opt/picard/lib/picard-2.6.0.jar
+        CLASSPATH=/opt/picard/lib/picard-2.6.0.jar:$CLASSPATH
         break
         ;;
     esac
